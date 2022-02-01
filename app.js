241d4c946cae04a2bcbdb100b5692090524a3ee4
Humairaa Saleh <humairaasaleh@gmail.com>
2022-02-01 12:10:31 +0000

    #FIN-32 creating login backend

--- app.js
+++ app.js
@@ -2,30 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const citizenRoute = require('./routes/citizenRoutes');
 
-
-
 const app = express();
-
-
 
 // COMMON MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use('/citizen', citizenRoute);
+app.use('/peopleBankAccount.js', peopleBankAccountRoute);
 
 
 
-
-
-
+// app.use('/users.js', userRoute)
 
 // ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(500).send(err.message);
 })
-
-
 
 // DIFFERENT DATABASE CONNECTIONS, FOR TESTING AND PRODUCTION
 let DBname = `<put DB name in here>`
