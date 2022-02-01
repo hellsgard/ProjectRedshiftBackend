97104418131e73b5dfdb4e9c90264cedef4201ca
Katie Diggory <Katie.diggory@gmail.com>
2022-02-01 16:19:44 +0000

    connecting front end to backend

--- routes/peopleBankAccountRoutes.js
+++ routes/peopleBankAccountRoutes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 const BankAccount = require('../models/peopleBankAccount.js');
+const { QueryTypes } = require('sequelize');
+const sequelize = require('../utils/database.js');
+
+router.get('/readAll', async (req, res) => {
+    const users = await sequelize.query("SELECT * FROM peoplebankaccount LIMIT 10", { type: sequelize.QueryTypes.SELECT } ); 
+    console.log(users);
+    res.status(200).send(users);
+    });
+
 
 
 
