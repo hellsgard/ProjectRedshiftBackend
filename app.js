34b7c2a522d01ec4727563f96e82d7303adb6bfa
chrisjones31419 <chrisleonjones@gmail.com>
2022-02-01 10:00:23 +0000

    #FIN-20 connecting back-end to database

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const citizenRoute = require('./routes/citizenRoutes');
+
+
+
+const app = express();
+
+
+
+// COMMON MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use('/citizen', citizenRoute);
+
+
+
+
+
+
+
+// ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send(err.message);
+})
+
+
+
+// DIFFERENT DATABASE CONNECTIONS, FOR TESTING AND PRODUCTION
+let DBname = `<put DB name in here>`
+// let DBname = `<Put test DB name in here>`  //                <- Test DB
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(8080, () => {
+  console.log(`Server is running on port ${server.address().port}.`);
+});
+
+module.exports = server;
