ad9d600bd62c486aee8a8e90410efa25c7c4b1f8
Katie Diggory <Katie.diggory@gmail.com>
2022-02-01 14:34:13 +0000

    Getting get req working in sequelize

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const citizenRoute = require('./routes/citizenRoutes');
+const citizenRoute = require('./routes/citizenRoutes.js');
+const peopleBankAccountRoutes = require('./routes/peopleBankAccountRoutes.js');
+const sequelize = require ('./utils/database');
+const userRoute = require ('./routes/userRoute');
+
 
 const app = express();
 
 // COMMON MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+// ROUTES
 app.use('/citizen', citizenRoute);
-app.use('/peopleBankAccount.js', peopleBankAccountRoute);
+app.use('/peopleBankAccount', peopleBankAccountRoutes);
+app.use('/users', userRoute)
 
+//database connection
+try {
+   sequelize.authenticate();
+  console.log('Connected to DB');
+} catch (error) {
+  console.error('Cant connect to DB')
+}
 
-
-// app.use('/users.js', userRoute)
+app.get("/hello", () => {
+  return console.log("site accessed");
+});
 
 // ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
@@ -20,27 +35,9 @@ app.use((err, req, res, next) => {
     res.status(500).send(err.message);
 })
 
-// DIFFERENT DATABASE CONNECTIONS, FOR TESTING AND PRODUCTION
-let DBname = `<put DB name in here>`
-// let DBname = `<Put test DB name in here>`  //                <- Test DB
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // DIFFERENT DATABASE CONNECTIONS, FOR TESTING AND PRODUCTION
+// let DBname = `<put DB name in here>`
+// // let DBname = `<Put test DB name in here>`  //                <- Test DB
 
 const server = app.listen(8080, () => {
   console.log(`Server is running on port ${server.address().port}.`);
