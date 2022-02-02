1a64f1f6d45769e35ceec58003e5c2c8c1f99d85
Katie Diggory <Katie.diggory@gmail.com>
2022-02-02 12:42:01 +0000

    front end and backend communication complete for 3 scenarios

--- routes/queryPersonRoute.js
+++ routes/queryPersonRoute.js
@@ -10,7 +10,6 @@ router.get('/person', async(req, res) => {
     const suspect = await sequelize.query("SELECT * FROM citizen WHERE surname=? AND forenames=? AND dateOfBirth=?", {replacements: [req.query.surname, req.query.forenames, req.query.dateOfBirth],
         type: QueryTypes.SELECT});
         console.log(suspect);
-   
 })
 
  // file where we do the filtering 
