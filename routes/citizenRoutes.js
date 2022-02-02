2b0104d1549b528275db51b47841f45f484862e8
Katie Diggory <Katie.diggory@gmail.com>
2022-02-02 09:32:36 +0000

    Working on citizen routes prepared statement

--- routes/citizenRoutes.js
+++ routes/citizenRoutes.js
@@ -4,9 +4,16 @@ const { QueryTypes } = require('sequelize');
 const sequelize = require('../utils/database.js');
 
 router.get('/readAll', async (req, res) => {
-    const users = await sequelize.query("SELECT * FROM citizen 0kLIMIT 10", { type: sequelize.QueryTypes.SELECT } ); // this will need changing back to citizen
+    const users = await sequelize.query("SELECT * FROM citizen10k LIMIT 10", { type: sequelize.QueryTypes.SELECT } ); // this will need changing back to citizen
     console.log(users);
     });
+
+router.get('/readPerson', async (req, res) => {
+    const person = await sequelize.query("SELECT * FROM citizen WHERE surname=?, forenames=?, dateOfBirth=?", {replacements: [$surname, $forename, $dateOfBirth],
+    type: QueryTypes.SELECT});
+})
+
+
 
 
 // const findAll = Citizen.findAll()
