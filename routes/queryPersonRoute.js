739df6168d89ea6fc9f0bb10debb9a8f94885b7f
chrisjones31419 <chrisleonjones@gmail.com>
2022-02-02 18:33:21 +0000

    added like to search suspect function

--- routes/queryPersonRoute.js
+++ routes/queryPersonRoute.js
@@ -5,11 +5,20 @@ const sequelize = require('../utils/database.js');
 
 
 
+// router.get('/person', async(req, res) => {
+//     console.log(req.query)
+//     const suspect = await sequelize.query("SELECT * FROM citizen WHERE surname=? AND forenames=? AND dateOfBirth=?", {replacements: [req.query.surname, req.query.forenames, req.query.dateOfBirth],
+//         type: QueryTypes.SELECT});
+//         console.log(suspect);
+// })
+
 router.get('/person', async(req, res) => {
     console.log(req.query)
-    const suspect = await sequelize.query("SELECT * FROM citizen WHERE surname=? AND forenames=? AND dateOfBirth=?", {replacements: [req.query.surname, req.query.forenames, req.query.dateOfBirth],
-        type: QueryTypes.SELECT});
-        console.log(suspect);
+    const suspect = await sequelize.query(`SELECT * FROM citizen WHERE surname like '%${req.query.surname}%' AND forenames like '%${req.query.forenames}%'  
+        AND dateOfBirth like '%${req.query.dateOfBirth}%'`, {replacements: [req.query.surname, req.query.forenames, req.query.dateOfBirth],
+            type: QueryTypes.SELECT});
+                console.log(suspect);
+    res.status(200).send(suspect);
 })
 
  // file where we do the filtering 
