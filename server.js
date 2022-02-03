6cff7e882b203fc4fa98f208337d6877375b82eb
Katie Diggory <Katie.diggory@gmail.com>
2022-02-03 09:27:54 +0000

    Working on passport

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const citizenRoute = require('./routes/citizenRoutes.js');
 const peopleBankAccountRoutes = require('./routes/peopleBankAccountRoutes.js');
-const sequelize = require ('./utils/database');
 const userRoute = require ('./routes/userRoute');
-
 const atmPointRoute = require ('./routes/atmPointRoute');
 const queryPersonRoute = require('./routes/queryPersonRoute');
-
 const atmTransactionsRoute = require('./routes/atmTransactionsRoute.js')
-
 const vehiclesRoute = require('./routes/vehiclesRoute.js')
 const bankCardRoutes = require ('./routes/bankCardRoutes')
 const anprCameraRoute = require ('./routes/anprCameraRoute');
 const queryIncidentRoute = require('./routes/queryIncidentRoute.js');
 const querySuspectFleesRoute = require('./routes/querySuspectFleesRoute.js');
+const passport = require('passport');
+const flash = require('express-flash'); 
+const session = require('express-session'); 
 
 const app = express();
 
@@ -29,34 +28,38 @@ app.use('/users', userRoute)
 app.use('/bankCard', bankCardRoutes);
 app.use('/atmTransactions', atmTransactionsRoute)
 app.use('/atmPoint', atmPointRoute);
-
-
 app.use('/queryPerson', queryPersonRoute);
 app.use('/queryIncident', queryIncidentRoute);
 app.use('/queryFlees', querySuspectFleesRoute);
-
 app.use('./vehicles', vehiclesRoute)
 app.use('/anprCamera', anprCameraRoute);
 
 
-//database connection
-try {
-  sequelize.authenticate();
-  console.log('Connected to DB');
-} catch (error) {
-  console.error('Cant connect to DB')
-}
+// PASSPORT
+const init = require('./passport-config.js');
+const UserSchema = require('./models/users.js');
 
-app.get("/hello", () => {
-  return console.log("site accessed");
+init(passport, username => {
+  return UserSchema.find(user => user.username === username)
 });
 
-// ERROR HANDLING MIDDLEWARE
-app.use((err, req, res, next) => {
-    console.log(err.stack);
-    res.status(500).send(err.message);
-})
+app.use(flash());
+app.use(session({
+  secret: 'the secret', 
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 100 * 60 * 60 }, // I think this is a 1hr expiry for cookies
+}))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+//whenever an app.post comes in this middleware should run
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/home',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 const server = app.listen(8080, () => {
   console.log(`Server is running on port ${server.address().port}.`);
