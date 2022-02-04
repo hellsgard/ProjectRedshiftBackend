56112d9a9f34328778a0edb23b5cba72bee2d012
Helen Gardner <hellsgard@gmail.com>
2022-02-04 17:48:21 +0000

    added atm query for map

--- routes/suspectProfile.js
+++ routes/suspectProfile.js
@@ -11,12 +11,31 @@ router.get('/byID/', async (req,res) => {
         {replacements: [req.params.id], type: QueryTypes.SELECT });
         console.log(info);
 
+    const atmLoc = await sequelize.query(
+        `SELECT ap.streetName, ap.postcode, ap.latitude, ap.longitude
+        FROM citizen ci JOIN peoplebankaccount pb ON pb.forenames=ci.forenames AND pb.surname=ci.surname AND pb.dateOfBirth=ci.dateOfBirth
+        JOIN bankcard ba ON pb.bankAccountId=ba.bankAccountId JOIN atmTransactions am ON ba.cardNumber=am.bankCardNumber
+        JOIN atmpoint ap ON am.atmId=ap.atmId 
+        WHERE ci.citizenID LIKE 1111657921`,
+        {replacements: [req.params.id], type: QueryTypes.SELECT });
+        console.log(atmLoc);
+
 
     // const colleagues = await sequelize.query(
     //     `SELECT * FROM peoplebusinessaddress WHERE businessName IN (SELECT businessName FROM peoplebusinessaddress WHERE ci.citizenID 
     //     LIKE ?`,
     //     {replacements: [req.query.citizenID], type: QueryTypes.SELECT });
     //     console.log(colleagues);
+    
+    // all ATM transactions with location based on ID
+//     SELECT ba.cardNumber, am.atmId, am.timestamp, am.amount, ap.operator, ap.streetName, ap.postcode, ap.latitude, ap.longitude
+// FROM citizen ci JOIN peoplebankaccount pb ON pb.forenames=ci.forenames AND pb.surname=ci.surname AND pb.dateOfBirth=ci.dateOfBirth
+// JOIN bankcard ba ON pb.bankAccountId=ba.bankAccountId JOIN atmTransactions am ON ba.cardNumber=am.bankCardNumber
+// JOIN atmpoint ap ON am.atmId=ap.atmId 
+// WHERE ci.citizenID LIKE ?;
+   
+
+
     
 
     // const family = await sequelize.query(
