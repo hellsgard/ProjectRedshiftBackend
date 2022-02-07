915e445fbc0178c19e07b0d0b31a4513e8ecc3d5
josho563 <93252771+josho563@users.noreply.github.com>
2022-02-07 12:42:47 +0000

    Delete database.js

--- utils/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize ('redshift', 'admin', 'Wireframe123!', {
-    dialect: 'mysql',
-    host: 'redshiftdev.cpd8frupo8ft.eu-west-2.rds.amazonaws.com',
-});
-
-
-module.exports = sequelize;
