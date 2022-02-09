30a1276f3ae853462db09539d4490435176a292e
chrisjones31419 <chrisleonjones@gmail.com>
2022-02-09 10:59:31 +0000

    added query incident vehcile route

--- routes/queryIncidentRoute.js
+++ routes/queryIncidentRoute.js
@@ -4,12 +4,21 @@ const { QueryTypes } = require('sequelize');
 const sequelize = require('../utils/database.js');
 
 
-
-router.get('/incident', async(req, res) => {
-    console.log(req.query);
-    const suspect = await sequelize.query("SELECT * FROM eposTransactions WHERE timestamp=?", {replacements: [req.query.timeDate],
+router.get('/incidentVehicle', async(req, res) => {
+    try {
+        console.log(req.query);
+        const vehicles = await sequelize.query(`SELECT c.citizenID, c.forenames, c.surname, c.homeAddress, c.dateOfBirth, c.placeOfBirth, c.sex, r.registrationID, r.registrationDate, o.vehicleRegistrationNumber, r.make,
+        r.model, r.colour, r.driverLicenceID, a.anprId, o.timestamp, a.streetName, a.latitude, a.longitude FROM citizen c 
+       JOIN vehicleRegistration r ON c.forenames=r.forenames AND c.surname=r.surname AND c.dateOfBirth=r.dateOfBirth
+       JOIN vehicleObservations o ON o.vehicleRegistrationNumber=r.vehicleRegistrationNo JOIN anprcamera a ON a.anprId=o.ANPRPointId
+       WHERE a.latitude LIKE 'rsr${req.query.latitude}%' AND a.longitude LIKE '${req.query.longitude}%' AND o.timestamp LIKE '${req.query.timeDate}%';`, {replacements: [req.query.latitude, req.query.longitude, req.query.timeDate],
         type: QueryTypes.SELECT});
-        console.log(suspect);
+        console.log("incident Vehicle search done");
+        res.status(200).send(vehicles);
+    }
+    catch (error) {
+        console.log(error);
+    }
 })
 
  // file where we do the filtering 
