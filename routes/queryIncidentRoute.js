628094f77be5ffae90e7ca211dd25018fc8bc96e
chrisjones31419 <chrisleonjones@gmail.com>
2022-02-09 20:05:39 +0000

    added scenario 2 routes

--- routes/queryIncidentRoute.js
+++ routes/queryIncidentRoute.js
@@ -11,14 +11,36 @@ router.get('/incidentVehicle', async(req, res) => {
         r.model, r.colour, r.driverLicenceID, a.anprId, o.timestamp, a.streetName, a.latitude, a.longitude FROM citizen c 
        JOIN vehicleRegistration r ON c.forenames=r.forenames AND c.surname=r.surname AND c.dateOfBirth=r.dateOfBirth
        JOIN vehicleObservations o ON o.vehicleRegistrationNumber=r.vehicleRegistrationNo JOIN anprcamera a ON a.anprId=o.ANPRPointId
-       WHERE a.latitude LIKE '${req.query.latitude}%' AND a.longitude LIKE '${req.query.longitude}%' AND o.timestamp LIKE '${req.query.timeDate}%';`, {replacements: [req.query.latitude, req.query.longitude, req.query.timeDate],
+       WHERE a.latitude LIKE '${req.query.latitude}%' AND a.longitude LIKE '${req.query.longitude}%' AND o.timestamp LIKE '${req.query.timeDate}%' LIMIT 5;`, {replacements: [req.query.latitude, req.query.longitude, req.query.timeDate],
         type: QueryTypes.SELECT});
         console.log("incident Vehicle search done");
-        res.status(200).send(vehicles);
+
+        console.log(req.query);
+        const financialInfo = await sequelize.query(`SELECT * FROM 
+        atmpoint p JOIN atmTransactions t on p.atmId=t.atmId join bankcard b ON t.bankCardNumber=b.cardNumber
+        JOIN peoplebankaccount ON b.bankAccountId=peoplebankaccount.bankAccountId
+        WHERE p.latitude LIKE '${req.query.latitude}%' AND p.longitude LIKE '${req.query.longitude}%' AND t.timestamp LIKE '${req.query.timeDate}%' LIMIT 5;`, {replacements: [req.query.latitude, req.query.longitude, req.query.timeDate],
+        type: QueryTypes.SELECT});
+        console.log("incident finacial search done");
+
+        res.status(200).send([financialInfo, vehicles]);
     }
     catch (error) {
         console.log(error);
     }
+
+
+    // try {
+    //     console.log(req.query);
+    //     const financialInfo = await sequelize.query(`SELECT * FROM 
+    //     atmpoint;`, {replacements: [req.query.latitude, req.query.longitude, req.query.timeDate],
+    //     type: QueryTypes.SELECT});
+    //     console.log("incident finacial search done");
+    //     res.status(200).send(financialInfo);
+    // }
+    // catch (error) {
+    //     console.log(error);
+    // }
 })
 
  // file where we do the filtering 
