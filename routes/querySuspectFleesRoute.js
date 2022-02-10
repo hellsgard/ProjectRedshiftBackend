25436cfd77d12772efdddde7fc5d715cfe0f0349
Katie Diggory <Katie.diggory@gmail.com>
2022-02-10 20:51:24 +0000

    SCenario3 working

--- routes/querySuspectFleesRoute.js
+++ routes/querySuspectFleesRoute.js
@@ -6,22 +6,18 @@ const sequelize = require('../utils/database.js');
 router.get('/anpr', async (req, res, next) => {
     console.log("query starts");
     const eposInfo = await sequelize.query(
-        `SELECT ac.latitude, ac.longitude, ac.streetName, vo.vehicleRegistrationNumber, vr.make, vr.model, vr.colour, vr.driverLicenceID, vo.timestamp
+        `SELECT ac.latitude, ac.longitude, ac.streetName, vo.vehicleRegistrationNumber, vr.make, vr.model, vr.colour, vr.driverLicenceID, vo.timestamp, ci.forenames, ci.surname, ci.dateOfBirth
     FROM citizen ci JOIN vehicleRegistration vr ON vr.forenames=ci.forenames AND vr.surname=ci.surname AND vr.dateOfBirth=ci.dateOfBirth
     JOIN vehicleObservations vo ON vr.vehicleRegistrationNo= vo.vehicleRegistrationNumber
     JOIN anprcamera ac ON vo.ANPRPointId= ac.anprId
     WHERE vo.vehicleRegistrationNumber = ?;`,
         { replacements: [req.query.vehicleReg], type: QueryTypes.SELECT })
-        .then((res) => {
+       
             console.log("is scenario 3 stuff working?")
-            console.log(res);
+            console.log(eposInfo);
             console.log(req.query.vehicleReg);
             console.log("HERE!!");
-            res.send(eposInfo);
-        })
-         .catch((error) => {
-            next(error);
-         });
- });
+            res.send(eposInfo); 
+        });
 
 module.exports = router;
